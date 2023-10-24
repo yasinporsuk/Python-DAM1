a8ba8acd23ff14a49e5be6059f13dbdb36d974e8
yasinporsuk <yasinporsuk@gmail.com>
2023-10-24 10:26:07 +0200

    24 oct 10:26

--- pythonProject1/factorialrecursivo.py
+++ pythonProject1/factorialrecursivo.py
@@ -1,9 +1,12 @@
-def factorial(num):
+def numfactorial(num):
     if num == 0 or num == 1:
         resultado = 1
     elif num>1:
-        resultado = num*factorial(num-1)
+        resultado = num * numfactorial(num-1)
     return resultado
 
-factory=factorial(5)
-print(factory)
+def main():
+    num = int(input("introduzca un numero "))
+    print(numfactorial(num))
+
+main()
